9e1dfd8c8cc5439fcf19584d275a4b84487a01f3
taheralfayad <taheralfayad@gmail.com>
2025-05-15 20:59:15 -0400

    add navbar and chat bar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,10 +3,45 @@
 </script>
 
 <nav>
-    <a href="/">Home Screen</a>
-    <a href="/newpage">New Page</a>
+    <div class="link-container">
+        <a href="/">
+            Home Screen
+        </a>
+        <a href="/newpage">
+            New Page
+        </a>
+    </div>
+
 </nav>
 
 {@render children()}
 
 <h1>thank you for your attention to this matter</h1>
+
+
+<style>
+    nav {
+        background-color: rgb(89, 87, 87);
+    }
+    a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+        color: white;
+        font-weight: bold;
+        padding: 15px 32px;
+        border-radius: 4px;
+        transition: background-color 0.3s;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    a:hover {
+        background-color: #bb4f15;
+        text-decoration: none;
+    }
+    .link-container {
+        display: flex;
+        gap: 1rem;
+        padding: 1rem;
+    }
+</style>
--- frontend/src/routes/newpage/+page.svelte
+++ frontend/src/routes/newpage/+page.svelte
@@ -4,6 +4,49 @@
     let { data }: PageProps = $props();
 </script>
 
-<h1>hello, this is the route newpage</h1>
 
-<p>{data.get.message}</p>
+
+<div class="search-bar-container">
+    <div class="search-bar">
+        <input
+            type="text"
+            placeholder="Visualize your data" 
+            class="search-input"
+        />
+    </div>
+    <button class="search-button">
+        Search
+    </button>
+</div>
+
+
+<style>
+    .search-bar-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 85vh;
+    }
+
+    .search-input {
+        padding: 10px;
+        width: 800px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+    .search-button {
+        padding: 10px 20px;
+        margin-left: 10px;
+        background-color: #bb4f15;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    .search-button:hover {
+        background-color: #a03e12;
+        transition: background-color 0.3s;
+    }
+</style>
